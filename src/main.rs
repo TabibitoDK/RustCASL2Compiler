ecafe60670a0d16ce8caa3cc5873bfc7160bda39
JeorgeC <jcteoh88@gmail.com>
2025-05-20 00:32:09 +0900

    update first functions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, world!");
+
+#[derive(Copy, Clone, Debug)]
+struct Register {
+    data: i16,
 }
+
+
+
+
+
+fn main() {
+    let mut GR: [Register; 8] = [Register { data: 0 }; 8];
+    let mut MEM: [i16; 65536] = [0; 8];
+
+    let mut a = 10;
+    let b = 42;
+    CASL2_LD(&mut a, &b);
+    println!("a = {}", a); // Output: a = 42
+}
+
+
+
+// LD Load opererater
+// LD r1 r2
+// CASL2_LD(&mut a, &b);
+fn CASL2_LD<T: Copy>(a: &mut T, b: &T) {
+    *a = *b;
+}
+
+
+
+// ST Store opererater
+// ST r, adr [,x]
+// CASL2_LD(&mut a, &b);
+fn CASL2_ST<T: Copy>(a: &mut T, b: &T) {
+    *a = *b;
+}
+
+
